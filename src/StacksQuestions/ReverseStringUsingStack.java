01b6671f29f2031523fda4a628309b946a0aa2bb
Kumar Mayank <kumarmayank@Kumars-MacBook-Pro.local>
2023-10-02 15:51:21 +0530

    Reverse_String_Using_Stack

---
+++ src/StacksQuestions/ReverseStringUsingStack.java
@@ -0,0 +1,25 @@
+package StacksQuestions;
+import java.util.Stack;
+
+public class ReverseStringUsingStack {
+    public static void main(String[] args){
+        String str="evol";
+        System.out.println("The reverse of String " +str+ " is-->"+ reverseStringUsingStack(str));
+
+    }
+
+    public static String reverseStringUsingStack(String str){
+        Stack<Character> st=new Stack<>();
+        String reversedString="";
+        for(int i=0;i<str.length();i++){
+            st.push(str.charAt(i));
+        }
+        int stack_size=st.size();
+        for(int j=0;j<stack_size;j++){
+            reversedString=reversedString+st.pop();
+        }
+        return reversedString;
+
+
+    }
+}
